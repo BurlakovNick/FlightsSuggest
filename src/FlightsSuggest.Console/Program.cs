7f3ca2d1b5c4ba50d80aca3e5c379a6b8297d540
Burlakov Nick <burlakov.nick@gmail.com>
2018-05-29 21:52:49 +0500

    Learned to parse wall posts, thanks to VkNet

--- src/FlightsSuggest.Console/Program.cs
+++ src/FlightsSuggest.Console/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using VkNet;
+using VkNet.Model.RequestParams;
 
 namespace FlightsSuggest.ConsoleApp
 {
@@ -6,7 +10,40 @@ namespace FlightsSuggest.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var vkApi = new VkApi();
+
+            vkApi.Authorize(new ApiAuthParams
+            {
+                ApplicationId = (ulong)int.Parse(args[0]),
+                AccessToken = args[1]
+            });
+            Console.WriteLine(vkApi.Token);
+
+            var wall = vkApi.Wall.Get(new WallGetParams
+            {
+                Count = 5,
+                Offset = 0,
+                Domain = "vandroukiru",
+            });
+
+            var posts = wall
+                .WallPosts
+                .Select(x => new VkWallPost
+                {
+                    Id = x.Id,
+                    GroupId = x.OwnerId,
+                    Text = x.Text
+                })
+                .ToArray();
+
+            Console.WriteLine(JsonConvert.SerializeObject(posts));
         }
+    }
+
+    public class VkWallPost
+    {
+        public long? Id { get; set; }
+        public long? GroupId { get; set; }
+        public string Text { get; set; }
     }
 }
